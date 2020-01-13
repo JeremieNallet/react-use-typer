a4011ed2dc68f3cc18344888025cdd9f05217b8b
Jérémie Nallet <jeremienallet@gmail.com>
2020-01-13 22:13:35 +0100

    rm isEmpty func

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback, useRef } from "react";
-import { isEmpty, speed, getAnyText } from "./utils";
+import { speed, getAnyText } from "./utils";
 
 const useTyper = (
     words = [""],
